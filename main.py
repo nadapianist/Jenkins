871708a41f44cd3a787e790c2ce655c078ab9db1
nadapianist <100685922+nadapianist@users.noreply.github.com>
2025-03-03 10:44:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ def main():
     # Configure MLflow experiment
     experiment_name = "churn_prediction_pipeline"
     mlflow.set_experiment(experiment_name)
-    train_data = pd.read_csv('churn-bigml-80.csv')
-    test_data = pd.read_csv('churn-bigml-0.csv')
+    train_data = pd.read_csv('/var/lib/jenkins/workspace/SecondPipeline/churn-bigml-80.csv')
+
+    test_data = pd.read_csv('/var/lib/jenkins/workspace/SecondPipeline/churn-bigml-0.csv')
     # Configurer le logging
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
      
